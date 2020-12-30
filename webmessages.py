dbf3a7fe71e5073ca23c1aea69445007bc1aee24
juancho2908 <juanchercuelon@gmail.com>
2020-12-30 18:47:39 -0500

    Esta puesto el input en phone

--- webmessages.py
+++ webmessages.py
@@ -1,13 +1,13 @@
 import pyautogui as pg
 import time
 import webbrowser as web
-phone_no="+593 "
+phone_no=input("Ingrese el numero: ")
 parsedMessage=" "
 web.open('https://web.whatsapp.com/send?phone='+phone_no+'&text='+parsedMessage)
 time.sleep(8)
 for i in range(10):
-    pg.write('')
-    pg.press('enter')
-    print('Mensaje #'+str(i+1)+' enviado')
-    pass
+     pg.write('esto es una prueba')
+     pg.press('enter')
+     print('Mensaje #'+str(i+1)+' enviado')
+pass
 pg.alert('Bomba de mensajes finalizada')
